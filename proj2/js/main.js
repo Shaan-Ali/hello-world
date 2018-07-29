8cf2941319664866c346a7b393b17c163013ad5d
Shaan <shaanqaus@gmail.com>
2018-07-29 11:25:20 -0400

    Removing comments

--- proj2/js/main.js
+++ proj2/js/main.js
@@ -1,9 +1,3 @@
-/*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    Project 3 - CoinStats
-*/
-
 var margin = { left:80, right:100, top:50, bottom:100 },
     height = 500 - margin.top - margin.bottom, 
     width = 800 - margin.left - margin.right;
